59d43a06d5d9fc9c78414e657378917341fbb8d5
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2015-06-09 18:18:36 +0500

    Set packed option to 1 by default

--- settings/server.js
+++ settings/server.js
@@ -64,7 +64,7 @@ module.exports = function(manifest, installPath) {
         homeUrl: "/home",
         collab: true,
         installed: true,
-        packed: false,
+        packed: true,
         packedThemes: true,
         readonly: false,
         role: "a",
