9b93076204f6836677b27ee489296fcddb6806f5
Alexander Sinyagin <sinyagin.alexander@gmail.com>
2015-06-09 13:24:18 +0500

    Add ethergit.preview.browser.sandbox to the installation script

--- scripts/install-sdk.sh
+++ scripts/install-sdk.sh
@@ -128,6 +128,7 @@ installEthergitPlugins() {
     git clone https://github.com/etherj/ethergit.sandbox.git
     git clone https://github.com/etherj/ethergit.ethereum.sandbox.git
     git clone https://github.com/etherj/ethergit.guests.count.git
+    git clone https://github.com/etherj/ethergit.preview.browser.sandbox.git
     cd ../
 }
 
